7ffd792678297713cfce7e4694efd518b5d2d0d0
devkgn88 <devkgn88@gmail.com>
2025-01-05 19:32:55 +0900

    [kgn] 재생목록vo추가

--- lec02_stmt/src/com/gn/practice/model/vo/Play.java
+++ lec02_stmt/src/com/gn/practice/model/vo/Play.java
@@ -1,5 +1,48 @@
 package com.gn.practice.model.vo;
 
 public class Play {
+	private int playNo;
+	private int userNo;
+	private int songNo;
+	
+	public Play() {}
 
+	public Play(int playNo, int userNo, int songNo) {
+		this.playNo = playNo;
+		this.userNo = userNo;
+		this.songNo = songNo;
+	}
+
+	public int getPlayNo() {
+		return playNo;
+	}
+
+	public void setPlayNo(int playNo) {
+		this.playNo = playNo;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public int getSongNo() {
+		return songNo;
+	}
+
+	public void setSongNo(int songNo) {
+		this.songNo = songNo;
+	}
+
+	@Override
+	public String toString() {
+		return "[번호 : " + playNo 
+				+ ", 사용자 번호 : " + userNo 
+				+ ", 음악 번호 : " + songNo + "]";
+	}
+	
+	
 }
